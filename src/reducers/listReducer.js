de28cd3397a7c855f1d43590918640428b16cbb3
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-08 14:31:01 -0500

    Add the corresponding two actions in the reducer to add lists and cards to the state.

--- src/reducers/listReducer.js
+++ src/reducers/listReducer.js
@@ -1,3 +1,5 @@
+import { CONSTANTS } from '../actions';
+
 const initialState = [
   {
     title: 'Column One',
@@ -39,8 +41,29 @@ const initialState = [
 
 const listReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'case':
-      break;
+    case CONSTANTS.ADD_LIST:
+      const newList = {
+        title: action.payload.title,
+        cards: [],
+        id: Math.floor(Math.random() * 100 + 1),
+      };
+
+      return [...state, newList];
+    case CONSTANTS.ADD_CARD:
+      const lists = [...state];
+      const newCard = {
+        text: action.payload.text,
+        id: Math.floor(Math.random() * 100 + 1),
+      };
+
+      //lists[action.payload.listId].cards.push(newCard);
+      lists.forEach(list => {
+        if (list.id === action.payload.listId) {
+          list.cards.push(newCard);
+        }
+      });
+
+      return lists;
     default:
       return state;
   }
