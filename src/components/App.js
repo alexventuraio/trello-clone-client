de044ef45e58c5006876a394db21c2c27e153e59
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-08 10:30:41 -0500

    Return back to the old redux implementation.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from '../store';
 
 import './App.css';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <h2>Hello World</h2>
-      </div>
-    </Provider>
+    <div className="App">
+      <h2>Hello World</h2>
+    </div>
   );
 }
 
