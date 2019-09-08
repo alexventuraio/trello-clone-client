a17bab308269ee46713dc046f90070ee78e64dd4
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-08 09:47:54 -0500

    Mode the store and provider config from index to app component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello World</h2>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h2>Hello World</h2>
+      </div>
+    </Provider>
   );
 }
 
